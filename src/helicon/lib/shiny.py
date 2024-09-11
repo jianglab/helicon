2d597d01b1c3677cfdbb39f2cf93b70f247a341a
Wen Jiang <jiang12@purdue.edu>
2024-09-10 23:36:33 -0400

    support multi-selections and display-only modes

--- src/helicon/lib/shiny.py
+++ src/helicon/lib/shiny.py
@@ -1,96 +1,181 @@
 from shiny import reactive
-from shiny.express import ui, module
+from shiny.express import ui, module, render
+
 
 @module
-def image_select(input, output, session, label="Select Image(s):", images=[], display_image_labels=True, image_labels=[], image_size=128, image_border=2, gap=0):
+def image_select(
+    input,
+    output,
+    session,
+    label="Select Image(s):",
+    images=reactive.value([]),
+    display_image_labels=True,
+    image_labels=reactive.value([]),
+    image_size=reactive.value(128),
+    auto_select_first=False,
+    disable_selection=False,
+    image_border=2,
+    gap=0,
+):
     import numpy as np
     from PIL import Image
     from helicon import encode_numpy, encode_PIL_Image
-    images_final = []
-    for i, image in enumerate(images):
-        if isinstance(image, str):
-            tmp = image
-        elif isinstance(image, Image.Image):
-            tmp = encode_PIL_Image(image)
-        elif isinstance(image, np.ndarray) and image.ndim == 2:            
-            tmp = encode_numpy(image)
-        else:
-            raise ValueError("image must be an image file, a PIL Image, or a 2D numpy array")
-        images_final.append(tmp)
 
-    assert len(image_labels)==0 or len(image_labels) == len(images)
-
-    if len(image_labels):
-        image_labels_final = image_labels
-    else:
-        image_labels_final = list(range(1, len(images)+1))
-
-    bids = []
-    
-    with ui.hold() as ui_imags:
-        with ui.div(style=f"display: flex; flex-flow: row wrap; justify-content: center; justify-items: center; align-items: center; gap: {gap}px {gap}px; margin: 0 0 {image_border}px 0"):
-            for i, image in enumerate(images_final):
-                    bid = f"image_select_{i+1}"
-                    bids.append(bid)
-
-                    with ui.hold() as img:
-                        ui.img(
-                            src=image,
-                            alt=f"Image {i+1}",
-                            title=str(image_labels_final[i]),
-                            style=f"object-fit: contain; max-width: {image_size-image_border*2}px; max-height: {image_size-image_border*2}px; border: {image_border}px solid transparent;"
-                        )
-
-                    ui.input_action_button(
-                        id=bid,
-                        label=
-                            ui.div(
-                                img,
-                                ui.p(image_labels_final[i], style="text-align: left; color: white; text-shadow: -1px -1px 0.5px rgba(0,0,0,0.5), 1px -1px 0.5px rgba(0,0,0,0.5), -1px 1px 0.5px rgba(0,0,0,0.5), 1px 1px 0.5px rgba(0,0,0,0.5); position: absolute; top: 2px; left: 5px;"),
-                                style="position: relative;"
-                            ) if display_image_labels else
-                            img,
-                        style=f"padding: 0px; border: 0px; margin: 0px; background-color: transparent;",
-                        onmouseover=f"if (this.querySelector('img').style.border !== '{image_border}px solid red') {{this.querySelector('img').style.border='{image_border}px solid blue'; this.querySelector('p').style.color='blue';}}",
-                        onmouseout =f"if (this.querySelector('img').style.border !== '{image_border}px solid red') {{this.querySelector('img').style.border='{image_border}px solid transparent';  this.querySelector('p').style.color='white';}}"
-                    )
-
-    if len(label):
-        ui.div(
-            ui.h4(label, style="text-align: center; margin: 0;"),
-            ui_imags,
-            style=f"display: flex; flex-direction: column; gap: {gap}px; margin: 0"
-        )
-    else:
-        ui_imags
-    
-    for bid in bids:
-        bid_js = f"{session.ns}-{bid}"
-        ui.tags.script(f'''
-            document.getElementById("{bid_js}").addEventListener("click", function() {{
-                var img  = document.getElementById("{bid_js}").querySelector("img");
-                var text = document.getElementById("{bid_js}").querySelector("p");
-                img.style.border = img.style.border === "{image_border}px solid red" ? "{image_border}px solid transparent" : "{image_border}px solid red";
-                if (text) {{
-                    text.style.color = img.style.border === "{image_border}px solid red" ? "red" : "white";
-                }}
-            }});
-        ''')
-
-    @reactive.Calc
-    def image_buttons_status():
-        return [input[bid]() % 2 == 1 for bid in bids]
-    
     selection = reactive.value([])
+    initial_selection = reactive.value([])
+    bids_react = reactive.value([])
 
-    @reactive.effect
+    @render.ui
+    def display_images():
+        if images() is None or len(images()) == 0:
+            return None
+
+        images_final = []
+        for i, image in enumerate(images()):
+            if isinstance(image, str):
+                tmp = image.copy()
+            elif isinstance(image, Image.Image):
+                tmp = encode_PIL_Image(image)
+            elif isinstance(image, np.ndarray) and image.ndim == 2:
+                tmp = encode_numpy(image)
+            else:
+                raise ValueError(
+                    "image must be an image file, a PIL Image, or a 2D numpy array"
+                )
+            images_final.append(tmp)
+
+        assert len(image_labels()) == 0 or len(image_labels()) == len(images_final)
+
+        if len(image_labels()):
+            image_labels_final = image_labels()
+        else:
+            image_labels_final = list(range(1, len(images_final) + 1))
+
+        assert image_size() >= 32
+
+        bids = [f"image_select_{i+1}" for i in range(len(images_final))]
+
+        def create_image_button(i, image, label, bid):
+            img = ui.img(
+                src=image,
+                alt=f"Image {i+1}",
+                title=str(label),
+                style=f"object-fit: contain; max-width: {image_size()-image_border*2}px; max-height: {image_size()-image_border*2}px; border: {image_border}px solid transparent;",
+            )
+
+            return ui.input_action_button(
+                id=bid,
+                label=(
+                    ui.div(
+                        img,
+                        ui.p(
+                            label,
+                            style="text-align: left; color: white; text-shadow: -1px -1px 0.5px rgba(0,0,0,0.5), 1px -1px 0.5px rgba(0,0,0,0.5), -1px 1px 0.5px rgba(0,0,0,0.5), 1px 1px 0.5px rgba(0,0,0,0.5); position: absolute; top: 2px; left: 5px;",
+                        ),
+                        style="position: relative;",
+                    )
+                    if display_image_labels
+                    else img
+                ),
+                disabled=disable_selection,
+                style=f"padding: 0px; border: 0px; margin: 0px; background-color: transparent;",
+                onmouseover=f"if (this.querySelector('img').style.border !== '{image_border}px solid red') {{this.querySelector('img').style.border='{image_border}px solid blue'; this.querySelector('p').style.color='blue';}}",
+                onmouseout=f"if (this.querySelector('img').style.border !== '{image_border}px solid red') {{this.querySelector('img').style.border='{image_border}px solid transparent';  this.querySelector('p').style.color='white';}}",
+                onclick=f"""var bid = '{session.ns}-{bid}';
+                            var count0 = parseInt(this.getAttribute('click_count')) || 0;
+                            count = count0 + 1
+                            var info = {{
+                                count: count,
+                                selected: (count)%2,
+                                ctrlKey: event.ctrlKey,
+                                shiftKey: event.shiftKey,
+                                altKey: event.altKey,
+                                metaKey: event.metaKey
+                            }};
+
+                            var img  = this.querySelector("img");
+                            var text = this.querySelector("p");
+                            img.style.border = count%2 ? "{image_border}px solid red" : "{image_border}px solid transparent";
+                            if (text) {{
+                                text.style.color = count%2 ? "red" : "white";
+                            }}
+
+                            this.setAttribute('click_count', count);
+                            //Shiny.setInputValue(bid, count, {{priority: 'deferred'}});
+                            Shiny.setInputValue(bid + '_click', info, {{priority: 'deferred'}});
+                            console.log("click", bid, parseInt(this.getAttribute('click_count')), info);
+                        """,
+            )
+
+        ui_images = ui.div(
+            *[
+                create_image_button(i, image, image_labels_final[i], bid)
+                for i, (image, bid) in enumerate(zip(images_final, bids))
+            ],
+            style=f"display: flex; flex-flow: row wrap; justify-content: center; justify-items: center; align-items: center; gap: {gap}px {gap}px; margin: 0 0 {image_border}px 0",
+        )
+
+        if len(label):
+            ui_images = ui.div(
+                ui.h5(
+                    label,
+                    style="text-align: center; margin: 0;",
+                    title="Hold the Shift key while clicking to select multiple classes",
+                ),
+                ui_images,
+                style=f"display: flex; flex-direction: column; gap: {gap}px; margin: 0",
+            )
+
+        bids_react.set(bids)
+        initial_selection.set([0] * len(bids))
+
+        if not disable_selection and auto_select_first:
+            tmp = initial_selection()
+            tmp[0] = 1
+            initial_selection.set(tmp)
+
+            click_scripts = []
+            click_scripts.append(
+                ui.tags.script(
+                    f"""
+                    var bid = '{session.ns}-{bids[0]}';
+                    document.getElementById(bid).click(); 
+                """
+                )
+            )
+            return (ui_images, click_scripts)
+        else:
+            return ui_images
+
+    @render.ui
     def ordered_selection():
-        status = image_buttons_status()
-        current = [i+1 for i, is_selected in enumerate(status) if is_selected]        
-        previous = getattr(ordered_selection, 'previous', [])        
+        status = [
+            (input[bid]() + initial_selection()[i]) % 2 == 1
+            for i, bid in enumerate(bids_react())
+        ]
+        current = [i for i, is_selected in enumerate(status) if is_selected]
+        previous = getattr(ordered_selection, "previous", [])
         result = [i for i in previous if i in current]
         result += [i for i in current if i not in result]
+        scripts = None
+        if len(result) > 1:
+            shiftKey = input[f"{bids_react()[result[-1]]}_click"]()["shiftKey"]
+            if not shiftKey:
+                scripts = []
+                for i in result[:-1]:
+                    bid = f"{session.ns}-{bids_react()[i]}"
+                    script = ui.tags.script(
+                        f"""document.getElementById('{bid}').click();"""
+                    )
+                    scripts.append(script)
+                result = [result[-1]]
+
         ordered_selection.previous = result
         selection.set(result)
-        
-    return selection
+
+        return scripts
+
+    if disable_selection:
+        return None
+    else:
+        return selection
