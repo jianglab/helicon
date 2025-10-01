fix calculation of rot angle sigma

src/helicon/commands/images2star.py
@@ -164,8 +164,6 @@ def main(args):
                 _, param_dict = helicon.parse_param_str(param)
             else:
                 param_dict = {}
-            
-            import os
 
             required_attrs = "rlnImageName rlnHelicalTubeID".split()
 
@@ -535,7 +533,7 @@ def main(args):
                 plt.show()
 
             index_d[option_name] += 1
-        
+
         elif option_name == "select" and len(param) == 2:
             var, val = param
             if var in data:
@@ -649,9 +647,7 @@ def main(args):
             col2 = param_dict.get("col2", "rlnImageName")
             assert col1 in data
             pattern = param_dict.get("pattern", None)
-            
-            import os
-            
+
             if not os.path.exists(sf):
                 helicon.color_print(
                     "\tERROR: option --selectFile=%s has specified a non-existent file %s"
@@ -673,8 +669,8 @@ def main(args):
 
             dids = convert_dataframe_file_path(data, col1, to="abs")
             sids = convert_dataframe_file_path(data_sf, col2, to="abs")
-            dids = dids.apply((lambda row: row.lstrip('0')))
-            sids = sids.apply((lambda row: row.lstrip('0')))
+            dids = dids.apply((lambda row: row.lstrip("0")))
+            sids = sids.apply((lambda row: row.lstrip("0")))
 
             if pattern:
                 dids = dids.str.extract(pattern, expand=False)
@@ -714,9 +710,7 @@ def main(args):
             sf, _ = helicon.parse_param_str(param)
             assert "rlnMicrographName" in data
             assert "rlnHelicalTubeID" in data
-            
-            import os
-            
+
             if sf is None or not os.path.exists(sf):
                 helicon.color_print(
                     "\tERROR: option --selectCommonHelices %s has specified a non-existent file %s"
@@ -856,94 +850,117 @@ def main(args):
                 _, param_dict = helicon.parse_param_str(param)
             else:
                 param_dict = {}
-            
-            if len(param.split(":"))>3:
+
+            if len(param.split(":")) > 3:
                 helicon.color_print(
                     "WARNING: here might be multiple selection criteria. Will use the intersection of them."
                 )
 
-            
             width = param_dict.get("width", None)
             outPath = param_dict.get("outPath", "./helicon.helices/")
             topLength = param_dict.get("topLength", None)
             topLengthFraction = param_dict.get("topLengthFraction", None)
             lengthCutoffAngst = param_dict.get("lengthCutoffAngst", None)
-            
-            import os
-            
+
             outPath = os.path.abspath(outPath)
-            
+
             import starfile
-            
-            get_apix=True
-            coord_df = pd.DataFrame(columns = ["startX", "startY", "endX", "endY", "rlnMicrographName", "helixLength"])
+
+            get_apix = True
+            coord_df = pd.DataFrame(
+                columns=[
+                    "startX",
+                    "startY",
+                    "endX",
+                    "endY",
+                    "rlnMicrographName",
+                    "helixLength",
+                ]
+            )
             for _, mic_name, coordfile in data.itertuples():
                 if get_apix:
                     import mrcfile
-                    with mrcfile.open(mic_name,'r') as mic:
+
+                    with mrcfile.open(mic_name, "r") as mic:
                         mic_data = np.array(mic.data, dtype=np.float32)
-                        apix=mic.voxel_size['x']
-                        get_apix=False
+                        apix = mic.voxel_size["x"]
+                        get_apix = False
                 cf = starfile.read(coordfile)
-                if cf is not None and not isinstance(cf,dict):
+                if cf is not None and not isinstance(cf, dict):
                     cf = cf.reset_index(drop=True)
-                    cf = cf.loc[:,["rlnCoordinateX", "rlnCoordinateY"]]
+                    cf = cf.loc[:, ["rlnCoordinateX", "rlnCoordinateY"]]
                     starts = cf.iloc[::2].reset_index(drop=True)
                     ends = cf.iloc[1::2].reset_index(drop=True)
-                    filaments = pd.DataFrame({
-                        'startX': starts['rlnCoordinateX'],
-                        'startY': starts['rlnCoordinateY'],
-                        'endX': ends['rlnCoordinateX'],
-                        'endY': ends['rlnCoordinateY'],
-                        'rlnMicrographName': mic_name
-                    })
-                    filaments['helixLength'] = np.sqrt((filaments['endX'] - filaments['startX']) ** 2 + 
-                               (filaments['endY'] - filaments['startY']) ** 2)
-                    #print(filaments)
-                    coord_df = pd.concat([coord_df,filaments])
+                    filaments = pd.DataFrame(
+                        {
+                            "startX": starts["rlnCoordinateX"],
+                            "startY": starts["rlnCoordinateY"],
+                            "endX": ends["rlnCoordinateX"],
+                            "endY": ends["rlnCoordinateY"],
+                            "rlnMicrographName": mic_name,
+                        }
+                    )
+                    filaments["helixLength"] = np.sqrt(
+                        (filaments["endX"] - filaments["startX"]) ** 2
+                        + (filaments["endY"] - filaments["startY"]) ** 2
+                    )
+                    # print(filaments)
+                    coord_df = pd.concat([coord_df, filaments])
 
-            coord_df['helixLength'] *= apix    
+            coord_df["helixLength"] *= apix
             coord_df = coord_df.sort_values(by="helixLength", ascending=False)
-            
+
             if topLengthFraction:
-                coord_df = coord_df.iloc[0:np.floor(len(coord_df)*topLengthFraction),:]
-            
+                coord_df = coord_df.iloc[
+                    0 : np.floor(len(coord_df) * topLengthFraction), :
+                ]
+
             if topLength:
                 if len(coord_df) > topLength:
-                    coord_df = coord_df.iloc[0:topLength,:]            
-            
+                    coord_df = coord_df.iloc[0:topLength, :]
+
             if lengthCutoffAngst:
-                coord_df = coord_df[coord_df['helixLength']>=lengthCutoffAngst]
-            
-            cpu=args.cpu
-            tasks=[]
-            helix_idx=0
+                coord_df = coord_df[coord_df["helixLength"] >= lengthCutoffAngst]
+
+            cpu = args.cpu
+            tasks = []
+            helix_idx = 0
             coord_df.reset_index(drop=True)
-            out_names=[]
-            for _,startX,startY,endX,endY,mic_name,_ in coord_df.itertuples():
-                mic_prefix='.'.join(mic_name.split('/')[-1].split('.')[:-1])
-                out_name = outPath+'/helix_'+str(helix_idx)+'_width_'+str(width)+'px_'+mic_prefix+'.mrc'
+            out_names = []
+            for _, startX, startY, endX, endY, mic_name, _ in coord_df.itertuples():
+                mic_prefix = ".".join(mic_name.split("/")[-1].split(".")[:-1])
+                out_name = (
+                    outPath
+                    + "/helix_"
+                    + str(helix_idx)
+                    + "_width_"
+                    + str(width)
+                    + "px_"
+                    + mic_prefix
+                    + ".mrc"
+                )
                 out_names.append(out_name)
-                tasks.append((startX,startY,endX,endY,mic_name,width,out_name))
-                helix_idx+=1
-            
+                tasks.append((startX, startY, endX, endY, mic_name, width, out_name))
+                helix_idx += 1
+
             coord_df["rlnHelixImageName"] = out_names
 
-            def process_one_task(startX,startY,endX,endY,mic_name,width,out_name):
+            def process_one_task(startX, startY, endX, endY, mic_name, width, out_name):
                 import mrcfile
-                with mrcfile.open(mic_name,'r') as mic:
+
+                with mrcfile.open(mic_name, "r") as mic:
                     mic_data = np.array(mic.data, dtype=np.float32)
-                    apix=mic.voxel_size['x']
-                    helix_image = helicon.get_rotated_clip(mic_data,startY,startX,endY,endX,width)
-                    with mrcfile.new(out_name,overwrite=True) as o_mrc:
-                        o_mrc.set_data(np.array(helix_image,dtype=np.float32))
-                        o_mrc.voxel_size=apix
-            
-            import os
-                
+                    apix = mic.voxel_size["x"]
+                    helix_image = helicon.get_rotated_clip(
+                        mic_data, startY, startX, endY, endX, width
+                    )
+                    with mrcfile.new(out_name, overwrite=True) as o_mrc:
+                        o_mrc.set_data(np.array(helix_image, dtype=np.float32))
+                        o_mrc.voxel_size = apix
+
             if not os.path.isdir(outPath):
                 os.makedirs(outPath)
-            
+
             from concurrent.futures import ThreadPoolExecutor, as_completed
 
             with ThreadPoolExecutor(max_workers=cpu) as executor:
@@ -954,9 +971,9 @@ def main(args):
                 for completed_task in as_completed(future_tasks):
                     result = completed_task.result()
                     results.append(result)
-            
+
             data = coord_df
-            #print(data)
+            # print(data)
 
         elif option_name == "sets" and param > 1:
             sets = param
@@ -1285,7 +1302,7 @@ def main(args):
 
         elif option_name == "replaceImageName" and param:
             replaceImageName = param
-            import os
+
             if not os.path.exists(replaceImageName):
                 helicon.color_print(("\tERROR: %s does not exist" % (replaceImageName)))
                 sys.exit(-1)
@@ -1324,7 +1341,7 @@ def main(args):
             mgraphs = micrographNames.groupby(micrographNames, sort=False)
 
             def setMicrographCTF(mgraphName, mgraphParticles, data, ctfparms):
-                import os
+
                 mid = os.path.basename(mgraphName)
                 mid = os.path.splitext(mid)[0]
                 mid2 = mid.split(".")[0]
@@ -1511,7 +1528,6 @@ def main(args):
         elif option_name == "path" and param != "current":
             path = param
             from helicon import convert_dataframe_file_path
-            import os
 
             for attr in "rlnImageName rlnMicrographName".split():
                 if attr in data:
@@ -1537,9 +1553,7 @@ def main(args):
             mgraphs = micrographNames.groupby(micrographNames, sort=False)
 
             count = 0
-            
-            import os
-            
+
             subdir = os.path.splitext(args.output_starFile)[0]
             if not os.path.isdir(subdir):
                 os.mkdir(subdir)
@@ -1621,9 +1635,7 @@ def main(args):
         elif option_name == "createStack" and param:
             # outputFile:rescale2size=<n>:float16=<0|1>
             outputFile, param_dict = helicon.parse_param_str(param)
-            
-            import os
-            
+
             if os.path.splitext(outputFile)[1] != ".mrcs":
                 suffix = Path(outputFile).suffix
                 helicon.color_print(
@@ -1895,9 +1907,7 @@ def main(args):
             )
             res_peak = 1 / R[np.argmax(pwr_mean)]
             apix_new = round(apix * target_res / res_peak, 3)  # precision: 0.1%
-            
-            import os
-            
+
             if args.verbose > 1:
                 outputFile = (
                     os.path.splitext(args.output_starFile)[0]
@@ -1975,9 +1985,7 @@ def main(args):
 
             mcount = 0
             d = helicon.EMData()
-            
-            import os
-            
+
             for mgraphName, mgraphParticles in mgraphs:
                 tmpdata = data.loc[mgraphParticles.index]
                 filename = tmpdata["rlnImageName"].iloc[0].split("@")[-1]
@@ -2147,7 +2155,7 @@ def main(args):
         elif option_name == "estimateHelicalAngleVariance" and param:
             missing_attrs = [
                 p
-                for p in "rlnImageName rlnHelicalTubeID rlnAngleTilt rlnAnglePsi rlnAngleRot".split()
+                for p in "rlnImageName rlnHelicalTubeID rlnHelicalTrackLengthAngst rlnAngleTilt rlnAnglePsi rlnAngleRot".split()
                 if p not in data
             ]
             assert (
@@ -2186,13 +2194,28 @@ def main(args):
                 psi = np.rad2deg(
                     np.arccos(np.cos(2 * np.deg2rad(psi)))
                 )  # to make the psi angles independent of polarity
-                psi_sigma = np.rad2deg(circstd(np.deg2rad(psi)))
+                psi_sigma = np.rad2deg(circstd(np.deg2rad(psi))) / 2
                 data.loc[group_particles.index, "rlnAnglePsiSigma"] = round(
                     psi_sigma, 2
                 )
                 psi_sigmas.append(psi_sigma)
                 rot = group_particles["rlnAngleRot"].astype(np.float32).values
-                rot_sigma = np.rad2deg(circstd(np.deg2rad(rot)))
+                if len(rot) > 1:
+                    pos = (
+                        group_particles["rlnHelicalTrackLengthAngst"]
+                        .astype(np.float32)
+                        .values
+                    )
+                    delta_rot = helicon.angular_difference(rot[1:], rot[:-1]) / (
+                        pos[1:] - pos[:-1]
+                    )
+                    # since rot angle should change linearly along the helical track, we cannot directly calculate the circular std of rot angles
+                    # instead, we calculate the circular std of delta_rot which represents the change of rot angle per Angstrom along the helical track
+                    # delta_rot should be a constant if there is no error in assigning rot angles
+                    # the unit is degree/Angstrom
+                    rot_sigma = np.rad2deg(circstd(np.deg2rad(delta_rot)))
+                else:
+                    rot_sigma = 0.0
                 data.loc[group_particles.index, "rlnAngleRotSigma"] = round(
                     rot_sigma, 2
                 )
@@ -2617,9 +2640,7 @@ def main(args):
             mgraphs = micrographNames.groupby(micrographNames, sort=False)
 
             count = 0
-            
-            import os
-            
+
             prefix = os.path.splitext(args.output_starFile)[0]
             for mgraphName, mgraphParticles in mgraphs:
                 tmpStarFile = "%s.%s.star" % (
@@ -2658,7 +2679,6 @@ def main(args):
 
     if args.path != "absolute":
         from helicon import get_relion_project_folder, convert_dataframe_file_path
-        import os
 
         relion_proj_folder = get_relion_project_folder(
             os.path.abspath(args.output_starFile)
@@ -2702,8 +2722,6 @@ def main(args):
             for si in range(args.splitNumSets):
                 subsets[si] = list(range(si, len(data), args.splitNumSets))
 
-        import os
-        
         prefix, suffix = os.path.splitext(args.output_starFile)
         for si, subset in enumerate(subsets):
             if args.splitNumSets == 2 and args.splitMode == "evenodd":
@@ -2724,9 +2742,7 @@ def main(args):
                 )
     else:
         helicon.dataframe2file(data, args.output_starFile)
-        
-        import os
-        
+
         if args.verbose:
             filename = ""
             for choice in "rlnImageName rlnMicrographName".split():
@@ -3648,9 +3664,7 @@ def check_args(args, parser):
             % (args.output_starFile)
         )
         sys.exit(-1)
-    
-    import os
-    
+
     if os.path.exists(args.output_starFile) and not (
         args.force == 1 or args.splitNumSets > 1
     ):